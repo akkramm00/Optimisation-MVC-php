c2045a0d09e5e750ca7651212ec20aadbf9b7498
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-02 09:58:37 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+<h1>Optimisation de l'utilisation du modèle MVC</h1>
+    <p>
+      Twig est un moteur de template de vue une syntaxe simple et expressive, vous pouvez générer du code HTML , XML ou tout autre format de sortie.
+      En utilisant Twig, vous pouvez créer facilement des modeles de vue réutilisables, ce qui rend votre code plus facile à aminteniret à mettre à jour.
+      Twig inclut de nembreuses fonctionnalités avancées telles que l'héritagede modèle, la composition de modèle, les blocs, les filtres , les ofnctrions, les itérateurs
+      , les conditions, les boucles et les expréssionsrégulières. Il vous permet également d'ajouter des extensions personnalisées pour étendre ses fonctionnalitées de base. En utilisant Twig avec le modèle de concéption MVC,vous pouvez rendre votre code plus modulaire et facile à maintenir.
+       Twig est largement utilisé dans de nombreux frameworks PHP populaires , tels symfony, laravel et Yii.
+    </p>
+  </body>
+</html>
